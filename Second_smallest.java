61a13bcb39ffec974dc19feeb870ecdc29c1466d
sukanya <sukanyalonkar57@gmail.com>
2020-03-05 21:41:44 +0530

    first sukanya commit in Master1

---
+++ Second_smallest.java
@@ -0,0 +1,21 @@
+package ex1;
+
+public class Second_smallest {
+	public static void main(String[] args) {
+		int []a={10,2,5,6,8,3,5,1};
+		for(int i=0;i<=a.length;i++){
+			for(int j=i+1;j<a.length;j++){
+				if(a[i]>a[j]){
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}	
+				}
+			}
+		System.out.println("Second largest Element: "+a[1]);
+
+	}
+
+
+
+}
